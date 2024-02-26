8e53f88e1c478cc29268eed5bb23932181f301e2
Lucas-Rechichi <157940317+Lucas-Rechichi@users.noreply.github.com>
2024-02-27 06:45:19 +1100

    uploaded 2 instances of the same file

--- Arabali/README.md
+++
@@ -1,1 +0,0 @@
-Link to my youtube channel: https://www.youtube.com/channel/UCPuNpdzS9GEJxLWwu2kB2jA
--- Arabali/sm_app/main/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from main import views as main_v
-urlpatterns = [
-    path('home/', main_v.home, name="home"),
-    path('page/', main_v.page, name="page"),
-]
