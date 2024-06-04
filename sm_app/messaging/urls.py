42af57ee3777e3de53cec030d54d3b74981abe49
Lucas-Rechichi <157940317+Lucas-Rechichi@users.noreply.github.com>
2024-06-05 06:33:27 +1000

    Video 14

--- sm_app/messaging/urls.py
+++ sm_app/messaging/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from messaging import views
 urlpatterns = [
-    path('chat/', views.chat_base, name='chat_base')
+    path('chat/', views.chat_base, name='chat_base'),
+
+    path('chat/<str:room>/<int:id>', views.chat_room_view, name='chat_room')
 ]
--- sm_app/messaging/views.py
+++ sm_app/messaging/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
-from messaging.extras import get_chat_rooms
 
+from main.models import UserStats
+from messaging.models import ChatRoom
+from messaging.extras import get_chat_rooms
+from main.extras import initialize_page
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 @login_required
 def chat_base(request):
     chat_rooms = get_chat_rooms(request.user)
+    init = initialize_page(request)
     variables = {
-        'chat_rooms': chat_rooms
+        'chat_rooms': chat_rooms,
+        'username': init['username'],
+        'search_bar': init['search_bar'],
     }
     return render(request, 'messaging/chat_base.html', variables)
+
+@login_required
+def chat_room_view(request, room, room_id):
+    # Initialize for HTML
+    chat_rooms = get_chat_rooms(request.user)
+    init = initialize_page(request)
+    
+    # Getting relevant database objects
+    user = request.user
+    rooms = ChatRoom.objects.filter(name=room)
+    chat_room = rooms.get(id=room_id)
+    user_stats = UserStats.objects.get(user=user)
+
+    # Checking to see if the user has been invited to this chat room.
+    if user_stats not in chat_room.users:
+        return render(request, 'main/error.html', {'issue': 'Cannot access this chatroom.'})
+
+    # Passing data over to HTML document
+    variables = {
+        'chat_rooms': chat_rooms,
+        'username': init['username'],
+        'search_bar': init['search_bar'],
+    }
+    return render(request, 'messaging/chat_room_view.html', variables)
