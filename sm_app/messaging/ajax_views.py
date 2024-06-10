faea074e8ce08de45b2a723760773d6aa1ce20a3
Lucas-Rechichi <157940317+Lucas-Rechichi@users.noreply.github.com>
2024-06-11 07:30:30 +1000

    Video 14

--- sm_app/messaging/ajax_views.py
+++ sm_app/messaging/ajax_views.py
@@ -1,3 +1,4 @@
+import pytz
 from django.http import JsonResponse
 from django.shortcuts import render
 from main.models import UserStats
@@ -18,14 +19,24 @@ def message_sent_text(request):
     chat_room = rooms.get(id=chat_room_id)
 
     # Creating the new Message and Saving it to the Database
-    new_message = Message(sender=user_stats, room=chat_room, text=text_message)
-    new_message.save()
+    if text_message:
+        new_message = Message(sender=user_stats, room=chat_room, text=text_message)
+        new_message.save()
+    else:
+        new_message = Message()
+
+    # Assuming you want to convert the datetime to a specific timezone
+    local_timezone = pytz.timezone("Australia/Sydney")
+    local_datetime = new_message.sent_at.astimezone(local_timezone)
+    
+    formatted_datetime = local_datetime.strftime("%B %d, %Y, %I:%M %p").lower().replace('am', 'a.m.').replace('pm', 'p.m.')
+    formatted_datetime_capitalized = formatted_datetime.capitalize()
     # Sending JSON responce
     responce = {
         'messageType':'text',
         'message': new_message.text,
         'message_user_pfp_url': new_message.sender.pfp.url,
-        'creationDate': new_message.sent_at,
+        'creationDate': formatted_datetime_capitalized,
     }
     return JsonResponse(responce)
 
